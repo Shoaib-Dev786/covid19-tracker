8a3d468fc601a58114aebc6551980e1be7a5ac5e
kmushtaq <kashifmushtaq114@gmail.com>
2021-09-09 17:12:09 +0500

    Footer Updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center align-center bg-blue-800 text-white p-6 w-full">
-        <span>Made by <b class="text-2xl font-bold">Shoaib</b> with ❤️</span>
+        <span>Made by <b class="text-2xl font-bold">Shoaib</b> with ❤️. Stay Safe 😷</span>
     </div>
 </template>
 
