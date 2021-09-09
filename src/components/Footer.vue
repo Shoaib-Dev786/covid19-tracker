e3e83e3bb8cbfc949b02f3d5b55890a06279224f
kmushtaq <kashifmushtaq114@gmail.com>
2021-09-09 15:25:38 +0500

    Footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center align-center bg-blue-800 text-white p-6 w-full">
+        <span>Made by <b class="text-2xl font-bold">Shoaib</b> with ❤️</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
